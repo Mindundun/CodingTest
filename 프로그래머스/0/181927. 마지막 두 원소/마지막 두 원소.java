bd6df6bf3dc2b02c51b07d8b46931e57e6d0eada
박민경 (Park MinKyung) <148737682+Mindundun@users.noreply.github.com>
2024-08-13 09:09:17 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -3,11 +3,13 @@ class Solution {
         int[] answer = new int[num_list.length+1];         
         
         System.arraycopy(num_list, 0, answer, 0, num_list.length); // 주어진 num_list룰 answer 배열에 복사
+        int after_int = num_list[num_list.length-1];
+        int before_int = num_list[num_list.length-2];
         
-        if (num_list[num_list.length-1] > num_list[num_list.length-2]) { //마지막 원소가 그전 원소보다 크면
-            answer[answer.length-1] =(num_list[num_list.length-1] - num_list[num_list.length-2]);//마지막 원소에서 그전 원소를 뺀 값
+        if (after_int > before_int) { //마지막 원소가 그전 원소보다 크면
+            answer[answer.length-1] =(after_int - before_int);//마지막 원소에서 그전 원소를 뺀 값
         } else {//마지막 원소가 그전 원소보다 크지 않다면
-            answer[answer.length-1] = (num_list[num_list.length-1] *2); //마지막 원소를 두 배한 값을 추가
+            answer[answer.length-1] = (after_int *2); //마지막 원소를 두 배한 값을 추가
         }
                 
         return answer;
