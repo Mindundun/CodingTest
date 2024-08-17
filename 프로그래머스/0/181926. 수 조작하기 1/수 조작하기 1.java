700c4a4833b321bacd86dcaad7fcb5a2c00688e6
박민경 (Park MinKyung) <148737682+Mindundun@users.noreply.github.com>
2024-08-17 10:54:12 +0900

    [level 0] Title: 수 조작하기 1, Time: 5.95 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -4,18 +4,10 @@ class Solution {
         
         for(int i = 0; i < control.length(); i++){
             switch (control.charAt(i)){
-                case 'w':
-                    n++;
-                    break;
-                case 's':
-                    n--;
-                    break;
-                case 'd':
-                    n+=10;
-                    break;
-                case 'a':
-                    n-=10;
-                    break;                    
+                case 'w': n++; break;
+                case 's': n--; break;
+                case 'd': n+=10; break;
+                case 'a': n-=10; break;                    
             }                   
         }
         
