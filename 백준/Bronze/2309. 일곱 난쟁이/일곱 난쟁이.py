7e0fa5b9e2c84cc5bc7026e19d4b562c0f4ea1c7
박민경 (Park MinKyung) <148737682+Mindundun@users.noreply.github.com>
2024-09-15 09:21:00 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,9 @@
+from itertools import combinations
+
+height = [int(input()) for _ in range(9)]
+
+for combi in combinations(height, 7):
+    if sum(combi) == 100:
+        for height in sorted(combi):
+            print(height)
+        break
