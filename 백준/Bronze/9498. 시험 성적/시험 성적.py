a40368671e183eaa8888a1f21f2a56313477b29c
박민경 (Park MinKyung) <148737682+Mindundun@users.noreply.github.com>
2024-09-24 21:44:15 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+input1 = int(input())
+
+if input1 > 89 :
+    print('A')
+elif input1 > 79 :
+    print('B')
+elif input1 > 69 :
+    print('C')
+elif input1 > 59 :
+    print('D')
+else:
+    print('F')
