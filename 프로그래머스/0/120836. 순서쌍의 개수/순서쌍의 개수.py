6b9012767c7d6ffee4ff0a6a0598d506c68921ae
박민경 (Park MinKyung) <148737682+Mindundun@users.noreply.github.com>
2025-08-01 14:06:00 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.15 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,21 +1,12 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
 def solution(n):
     answer = 0
-
-    # 1부터 n번째까지 반복
-    for i in range(1, n + 1):
-        # n을 반복된 숫자로 나눈 나머지가 0인 경우 나뉜다고 판단
+    
+    # 1부터 n의 제곱근까지 반복
+    for i in range(1, int(n ** 0.5) + 1):
+        # n을 반복된 숫자로 나눈 나머지가 0인 경우 나뉜다고 판단(제곱근하여 +2)
         if(n % i == 0):
-            answer += 1
-
+            answer += 2
+            # 만약 i제곱이 나눠진다면 -1 
+            if (i * i == n):
+                answer -= 1
     return answer
