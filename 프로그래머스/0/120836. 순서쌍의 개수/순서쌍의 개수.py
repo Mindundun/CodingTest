66553cc34f13bc8e15142feeca21e0dcd705ec14
박민경 (Park MinKyung) <148737682+Mindundun@users.noreply.github.com>
2025-08-01 13:58:34 +0900

    [level 0] Title: 순서쌍의 개수, Time: 71.91 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,10 +1,21 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
 def solution(n):
     answer = 0
-    
+
     # 1부터 n번째까지 반복
     for i in range(1, n + 1):
-        # n을 반복된 숫자로 나눈 나머지가 0 인 경우 나뉜다고 판단
+        # n을 반복된 숫자로 나눈 나머지가 0인 경우 나뉜다고 판단
         if(n % i == 0):
             answer += 1
-            
+
     return answer
