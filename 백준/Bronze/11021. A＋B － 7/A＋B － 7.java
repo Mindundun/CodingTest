0936a926fb3e7c714f725d1f8edc79a023f1dee0
박민경 (Park MinKyung) <148737682+Mindundun@users.noreply.github.com>
2025-11-13 17:28:44 +0900

    [Bronze V] Title: A+B - 7, Time: 120 ms, Memory: 15876 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 첫 줄에 합계 갯수 입력 받기
+        int N = Integer.parseInt(br.readLine());
+
+        // 출력할 변수 담기
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 1; i <= N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            sb.append("Case #"+ i +": ").append(A+B).append("\n");
+        }
+        System.out.println(sb);
+
+    }
+    
+}
