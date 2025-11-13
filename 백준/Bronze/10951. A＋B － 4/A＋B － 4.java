c7b05ded2ad2e3d6a2d365cf981bcb1f7c137c50
박민경 (Park MinKyung) <148737682+Mindundun@users.noreply.github.com>
2025-11-14 08:56:04 +0900

    [Bronze V] Title: A+B - 4, Time: 108 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String line;
+
+        while((line = br.readLine()) != null) {
+            String[] arr = line.split(" ");
+            int a = Integer.parseInt(arr[0]);
+            int b = Integer.parseInt(arr[1]);
+
+            System.out.println(a+b);
+        }
+    }    
+}
