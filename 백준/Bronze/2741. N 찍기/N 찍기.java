3627e6edc628b5b92175efc595f7ba76e80faf6e
박민경 (Park MinKyung) <148737682+Mindundun@users.noreply.github.com>
2025-11-13 15:07:55 +0900

    [Bronze V] Title: N 찍기, Time: 756 ms, Memory: 30176 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -1,14 +1,16 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int num = scan.nextInt();
-		
-		for(int i = 1 ; i <= num; i++) {
-			System.out.println(i);
-		}
-		scan.close();
-	}
+    public static void main(String[] args) throws Exception{
+        // 입력을 받기 위함.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 입력받은 값을 num에 넣음
+        int num = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= num ; i++){
+            System.out.println(i);
+        }
+    }
 }
