e0f9cfad8495e76fb0ab550248539b89e7259e26
박민경 (Park MinKyung) <148737682+Mindundun@users.noreply.github.com>
2025-11-16 18:59:45 +0900

    [Bronze V] Title: 꼬마 정민, Time: 96 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Long A = Long.parseLong(st.nextToken());
+        Long B = Long.parseLong(st.nextToken());
+        Long C = Long.parseLong(st.nextToken());
+
+        System.out.print(A+B+C);
+    }
+}
