d2f30e2fe7242009a3d8e440dfd7404b5d91d9a7
박민경 (Park MinKyung) <148737682+Mindundun@users.noreply.github.com>
2025-11-18 11:08:49 +0900

    [Bronze V] Title: 문자열, Time: 124 ms, Memory: 15868 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,21 +1,25 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int num = scan.nextInt();
-		scan.nextLine();
-		String arr[] = new String[num+1];
-		
-		//배열 입력
-		for(int i = 0 ; i < num; i++) {
-			arr[i] = scan.nextLine();
-		}
-		//배열 출력
-		for(int i = 0 ; i < num; i++) {	
-			System.out.println(arr[i].charAt(0) + "" + arr[i].charAt(arr[i].length()-1));
-		}
-		scan.close();
-	}
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        String[] arr = new String[num];
+
+        // 입력받은 문자열을 배열에 담는다.
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = br.readLine();
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            // 입력받은 문자열의 첫글자와 마지막글자를 문자열로 붙임.
+            // char + char는 숫자라서 앞에 ""를 더해 문자열처리로써 문자를 붙임
+            System.out.println("" + arr[i].charAt(0) + arr[i].charAt(arr[i].length() -1));
+        }     
+    }
+    
 }
